3f18185a98786165839c6614da356ba2a18e3d7a
Axyonov Maxim <maksik.aksyonov@mail.ru>
2025-09-24 23:22:18 +0500

    Add project files.

---
+++ Tyuiu.AxyonovMA.Sprint1.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AksyonovMA.Sprint1.Task0.V27.Lib
+{
+    public class DataService : ISprint1Task0V27
+    {
+        public double Calculate()
+        {
+            return 5 * 2 + 4 * 3;
+        }
+    }
+}
---
+++ Tyuiu.AxyonovMA.Sprint1.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.AksyonovMA.Sprint1.Task0.V27.Lib;
+
+namespace Tyuiu.AksyonovMA.Sprint1.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            var result = ds.Calculate();
+            Assert.AreEqual(22, result);
+        }
+    }
+}
